05ae04ea8479e15f2df3c11b07fad8f6beb5edff
emmettzyoung <emmett.zyoung@gmail.com>
2026-02-10 17:44:11 -0500

    Initial commit

---
+++ README.md
@@ -0,0 +1,2 @@
+# Lab2
+Emmett Young's GGR472H1 Lab 2 Repository
---
+++ script.js
@@ -0,0 +1,55 @@
+mapboxgl.accessToken = 'pk.eyJ1IjoiZW1tZXR0eW91bmciLCJhIjoiY21rNGI3Y3Z4MDV3ZjNrcHk2MXFrYTlpeSJ9.nhnMjjZj_o1eyXtp_Y8Svw';
+
+const map = new mapboxgl.Map({
+    container: 'my-map',
+    style: 'mapbox://styles/emmettyoung/cmlh2534n006s01qv686sadry',
+    center: [-79.38, 43.65],
+    zoom: 12
+});
+
+map.addControl(
+    new MapboxGeocoder({
+        container: 'geocoder',
+        accessToken: mapboxgl.accessToken,
+        useBrowserFocus: true,
+        mapboxgl: mapboxgl
+    })
+);
+
+
+map.on('load', () => {
+
+    // Add a data source containing GeoJSON data
+    map.addSource('uoft-data', {
+        type: 'geojson',
+        data: {
+            "type": "FeatureCollection",
+            "features": [
+                {
+                    "type": "Feature",
+                    "properties": {
+                        "name": "Sidney Smith Hall"
+                    },
+                    "geometry": {
+                        "coordinates": [
+                            -79.39865237301687,
+                            43.662343395037766
+                        ],
+                        "type": "Point"
+                    }
+                }
+            ]
+        }
+    }
+    );
+    // Visualize data layer on map
+    map.addLayer({
+        'id': 'uoft-pnt',
+        'type': 'circle',
+        'source': 'uoft-data',
+        'paint': {
+            'circle-radius': 6,
+            'circle-color': '#B42222'
+        }
+    });
+});
